a4fc75c5d3c56ad2734aaccf03107163f755dcf2
Fernandez-Sebastian <fernandez90dgo@gmail.com>
2025-10-05 02:37:26 -0300

    Se crea el formulario de login Se  agrega la conexion a la base de datos Se configura el boton de ingreso se configura el boton de salir

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Data;
+using System.Windows.Forms;
+using Proyecto_Integrador_Grupo_11_B.Datos;
+
 namespace Proyecto_Integrador_Grupo_11_B
 {
     public partial class Form1 : Form
@@ -5,6 +10,39 @@ namespace Proyecto_Integrador_Grupo_11_B
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPass.Text = "CONTRASEÑA";
+            txtPass.UseSystemPasswordChar = false;
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            txtPass.Text = "";
+            txtPass.UseSystemPasswordChar = true;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            DataTable tablaLogin = new DataTable();
+            Usuarios dato = new Usuarios();
+            tablaLogin = dato.Log_Usu(txtUsuario.Text, txtPass.Text);
+
+            if (tablaLogin.Rows.Count > 0)
+            {
+                MessageBox.Show("Ingreso exitoso");
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o password incorrecto");
+            }
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
