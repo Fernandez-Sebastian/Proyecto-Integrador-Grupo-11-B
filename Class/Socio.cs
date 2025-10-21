a676cc057bd3d86349e0996d62d215a15a84ebea
Fernandez-Sebastian <fernandez90dgo@gmail.com>
2025-10-21 00:33:15 -0300

    -Se encapsula la lógica de registro de socios dentro de la clase Socio.
    - Se actualizó el método RegistrarSocio() para que la clase maneje la inserción en BD.
    - Se agregó SetDatosPersona() para asignar propiedades de manera centralizada usando la clase heredada.
    - El formulario ahora solo setea propiedades y llama al método, manteniendo separación de responsabilidades y código más limpio.

---
+++ Class/Socio.cs
@@ -0,0 +1,86 @@
+﻿using MySql.Data.MySqlClient;
+using Proyecto_Integrador_Grupo_11_B.Datos;
+
+namespace Proyecto_Integrador_Grupo_11_B.Class
+{
+    internal class Socio : Persona
+    {
+        public DateTime FechaNacimiento { get; set; }
+        public string AptoMedico { get; set; }
+
+        public string RegistrarSocio()
+        {
+            try
+            {
+                // Creamos la conexión usando la clase Conexion.
+                // Al estar todo dentro de using si hay un error en la conexión o termina el bloque de codigo, la conexión finaliza.
+                using (MySqlConnection conn = Conexion.getInstancia().CrearConexion())
+                {
+                    conn.Open();
+
+                    string SQL = "INSERT INTO Socios (Dni, Nombre, Apellido, FechaNacimiento, AptoMedico) " +
+                                 "VALUES (@dni, @nombre, @apellido, @fechaNacimiento, @aptoMedico)";
+
+                    using (MySqlCommand cmd = new MySqlCommand(SQL, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@dni", Dni);
+                        cmd.Parameters.AddWithValue("@nombre", Nombre);
+                        cmd.Parameters.AddWithValue("@apellido", Apellido);
+                        cmd.Parameters.AddWithValue("@fechaNacimiento", FechaNacimiento.ToString("yyyy-MM-dd"));
+                        cmd.Parameters.AddWithValue("@aptoMedico", AptoMedico);
+
+                        int filas = cmd.ExecuteNonQuery();
+
+                        if (filas > 0)
+                        {
+                            return "OK";
+                        }
+                        return "No se pudo registrar el socio.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "ERROR: " + ex.Message;
+            }
+        }
+        
+        // Función que devuelve true o false si existe en la base de datos el DNI ingresado.
+        // Validar si el DNI ya existe
+        public string ExisteDni(string Dni)
+        {
+            try
+            {
+                using (MySqlConnection conn = Conexion.getInstancia().CrearConexion())
+                {
+                    conn.Open();
+                    string SQL = "SELECT COUNT(*) FROM Socios WHERE Dni = @dni";
+
+                    using (MySqlCommand cmd = new MySqlCommand(SQL, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@dni", Dni);
+                        int cantidad = Convert.ToInt32(cmd.ExecuteScalar());
+                        if(cantidad > 0)
+                        {
+                            return "S";
+                        }
+                        return "N";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "ERROR: " + ex.Message;
+            }
+        }
+
+        // Seteo los campos heredados
+        public void SetDatosPersona(string dni, string nombre, string apellido)
+        {
+            this.Dni = dni;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+
+    }
+}
--- RegistrarSocio.cs
+++ RegistrarSocio.cs
@@ -1,4 +1,4 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Proyecto_Integrador_Grupo_11_B.Class;
 
 namespace Proyecto_Integrador_Grupo_11_B
 {
@@ -26,79 +26,72 @@ namespace Proyecto_Integrador_Grupo_11_B
 
         private void Registrar_Click(object sender, EventArgs e)
         {
-            // Tomamos los valores del formulario
-            string dni = txtDni.Text.Trim();
-            string nombre = txtNombre.Text.Trim();
-            string apellido = txtApellido.Text.Trim();
-            DateTime fechaNacimiento = dtpFechaNacimiento.Value;
-            string aptoMedico = chkAptoMedico.Checked ? "S" : "N";
-
-            if (string.IsNullOrEmpty(dni))
-            {
-                MessageBox.Show("El campo DNI no puede estar vacío.");
-                return;
-            }
-            if (string.IsNullOrEmpty(nombre))
-            {
-                MessageBox.Show("El campo Nombre no puede estar vacío.");
-                return;
-            }
-            if (string.IsNullOrEmpty(apellido))
-            {
-                MessageBox.Show("El campo Apellido no puede estar vacío.");
-                return;
-            }
-
-            if (aptoMedico == "N")
-            {
-                MessageBox.Show("Para continuar el alta, debe presentar el Apto Médico.");
-                return;
-            }
-
-            int edad = DateTime.Today.Year - fechaNacimiento.Year;
-            // calcula el mes de nacimiento para determinar si ya cumplió o no los años.
-            if (fechaNacimiento.Date > DateTime.Today.AddYears(-edad)) edad--;
-
-            // La fecha del socio debe ser mayor que 5 años y menor que 100 años
-            // Además no se puede ingresar una edad mayor al día de hoy
-            if (fechaNacimiento > DateTime.Today)
-            {
-                MessageBox.Show("La fecha de nacimiento no puede ser futura.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            else if (edad < 5)
-            {
-                MessageBox.Show("El socio debe tener al menos 5 años.", "Edad no válida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            else if (edad > 100)
-            {
-                MessageBox.Show("La edad ingresada no es válida.", "Edad no válida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
             try
             {
-                // Creamos la conexión usando la clase Conexion
-                MySqlConnection conn = Datos.Conexion.getInstancia().CrearConexion();
-                conn.Open();
+                // Se crea una instancia la clase Socio.
+                Socio NuevoSocio = new();
 
-                // Validar si el DNI ya existe
-                string SQL = "SELECT COUNT(*) FROM Socios WHERE Dni = @dni";
-                using (MySqlCommand DniCheck = new MySqlCommand(SQL, conn))
+                // Tomamos los valores del formulario y lo asignamos a variables para hacer las validaciones correspondientes.
+                string dni = txtDni.Text.Trim();
+                string nombre = txtNombre.Text.Trim();
+                string apellido = txtApellido.Text.Trim();
+                DateTime fechaNacimiento = dtpFechaNacimiento.Value;
+                string aptoMedico = chkAptoMedico.Checked ? "S" : "N";
+
+                if (string.IsNullOrEmpty(dni))
                 {
-                    DniCheck.Parameters.AddWithValue("@dni", dni);
-                    int existe = Convert.ToInt32(DniCheck.ExecuteScalar());
-
-                    if (existe > 0)
-                    {
-                        MessageBox.Show("Ya existe un socio registrado con este DNI.", "Duplicado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        conn.Close();
-                        return;
-                    }
+                    MessageBox.Show("El campo DNI no puede estar vacío.");
+                    return;
+                }
+                if (string.IsNullOrEmpty(nombre))
+                {
+                    MessageBox.Show("El campo Nombre no puede estar vacío.");
+                    return;
+                }
+                if (string.IsNullOrEmpty(apellido))
+                {
+                    MessageBox.Show("El campo Apellido no puede estar vacío.");
+                    return;
                 }
 
-                // Agregamos alerta para confirmar el Nuevo alta de Socio
+                if (aptoMedico == "N")
+                {
+                    MessageBox.Show("Para continuar el alta, debe presentar el Apto Médico.");
+                    return;
+                }
+
+                int edad = DateTime.Today.Year - fechaNacimiento.Year;
+                // calcula el mes de nacimiento para determinar si ya cumplió o no los años.
+                if (fechaNacimiento.Date > DateTime.Today.AddYears(-edad)) edad--;
+
+                // La fecha del socio debe ser mayor que 5 años y menor que 100 años
+                // Además no se puede ingresar una edad mayor al día de hoy
+                if (fechaNacimiento > DateTime.Today)
+                {
+                    MessageBox.Show("La fecha de nacimiento no puede ser futura.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else if (edad < 5)
+                {
+                    MessageBox.Show("El socio debe tener al menos 5 años.", "Edad no válida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else if (edad > 100)
+                {
+                    MessageBox.Show("La edad ingresada no es válida.", "Edad no válida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Si llegue acá todos los datos ingresados ya estan correctos para realizar el alta del Socio.
+                // Valido que el DNI no este registardo.
+                if (NuevoSocio.ExisteDni(dni) == "S")
+                {
+                    MessageBox.Show($"Ya existe un socio registrado con el DNI {dni}.", "Duplicado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Si llegamos aca, el DNI ingresado no esta duplicado y los datos estan validados.
+                // Agregamos alerta para confirmar el Nuevo alta de Socio.
                 DialogResult result = MessageBox.Show(
                     $"¿Seguro que deseas dar de alta a {nombre} {apellido} DNI: {dni}? ",
                     "Confirmar salida",
@@ -108,46 +101,31 @@ namespace Proyecto_Integrador_Grupo_11_B
 
                 if (result == DialogResult.No)
                 {
-                    conn.Close();
                     return;
                 }
 
-                // Insert del nuevo Socio
-                SQL = "INSERT INTO Socios (Dni, Nombre, Apellido, `FechaNacimiento`, AptoMedico) " +
-                      "VALUES (@dni, @nombre, @apellido, @fechaNacimiento, @aptoMedico)";
+                // Carga de datos y registro el Socio en la base de datos.
+                NuevoSocio.SetDatosPersona(dni, nombre, apellido);
+                NuevoSocio.FechaNacimiento = fechaNacimiento;
+                NuevoSocio.AptoMedico = aptoMedico;
+                string EstadoAlta = NuevoSocio.RegistrarSocio();
 
-                using (MySqlCommand cmd = new MySqlCommand(SQL, conn))
+                if (EstadoAlta == "OK")
                 {
-                    cmd.Parameters.AddWithValue("@dni", dni);
-                    cmd.Parameters.AddWithValue("@nombre", nombre);
-                    cmd.Parameters.AddWithValue("@apellido", apellido);
-                    cmd.Parameters.AddWithValue("@fechaNacimiento", fechaNacimiento.ToString("yyyy-MM-dd"));
-                    cmd.Parameters.AddWithValue("@aptoMedico", aptoMedico);
-
-                    int filas = cmd.ExecuteNonQuery();
-
-                    if (filas > 0)
-                    {
-                        MessageBox.Show($"Socio {nombre} {apellido} registrado correctamente. Para realizar una actividad debe tener la cuota al día.");
-                        // limpiar campos
-                        txtDni.Clear();
-                        txtNombre.Clear();
-                        txtApellido.Clear();
-                        dtpFechaNacimiento.Value = DateTime.Today;
-                        chkAptoMedico.Checked = false;
-                        this.Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("No se pudo registrar el socio.");
-                    }
+                    MessageBox.Show($"\"Socio {nombre} {apellido} registrado correctamente. Para realizar una actividad debe tener la cuota al día.\"");
+                    this.Close();
                 }
-
-                conn.Close();
+                else
+                {
+                    MessageBox.Show("No se pudo registrar el socio.");
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error al registrar el socio: " + ex.Message);
+                MessageBox.Show($"Ocurrió un error inesperado durante el registro del socio:\n{ex.Message}",
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
             }
         }
 
